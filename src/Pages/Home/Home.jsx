161fc3ad6ae840c4edca6cea59280810ba11d2bb
aruna-kariyawasam <arunakariyawasam27862@gmail.com>
2025-02-05 12:01:45 +0530

    Add another layer to HP

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
+import HomeMidLayer from '../../Components/HomeMidLayer'
 
 const Home = () => {
   return (
      <div className="Medinexus-Home">
         <Header/>
+        <HomeMidLayer/>
     </div>
   )
 }
