a29822fd5244c41bf87ae2b9cabd475e9ae87af7
aruna-kariyawasam <arunakariyawasam27862@gmail.com>
2025-03-07 13:02:13 +0530

    home changed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -18,6 +18,7 @@ const chunkArray = (arr, size) => {
   return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
     arr.slice(i * size, i * size + size)
   );
+  
 };
 
 const Home = () => {
